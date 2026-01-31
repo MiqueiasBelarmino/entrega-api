dc81905f51d090a91ca87efaa862f92025391842
Miquéias Belarmino <miqueiascostabac@gmail.com>
2026-01-31 14:55:50 -0300

    feat: twilio SMS integration

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { NotificationSender } from './notification-channel';
 import { ConsoleNotificationSender } from './console-notification-sender';
+import { TwilioNotificationSender } from './twilio-notification-sender';
 
 @Module({
   providers: [
     {
       provide: NotificationSender,
-      useClass: ConsoleNotificationSender,
+      useClass:
+        process.env.NODE_ENV === 'production'
+          ? TwilioNotificationSender
+          : TwilioNotificationSender, // Using Twilio for dev too as requested, or can be toggled
     },
   ],
   exports: [NotificationSender],
